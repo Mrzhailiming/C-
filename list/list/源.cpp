ed6e28b07e62d91c50eaa3861ff695b44e70716b
Mrzhailiming <401875751@qq.com>
2019-11-17 17:38:32 +0800

    2019-11-17 17:38:29

--- list/list/源.cpp
+++ list/list/源.cpp
@@ -20,9 +20,40 @@ public:
 };
 
 //迭代器
-template <class T>
+template <class T, class ref, class ptr>
 class ListIterator{
+	typedef ListNode<T> Node;
 	typedef ListNode<T>* pNode;
+	typedef ListIterator<T, ref, ptr> self;
+
+	pNode _node;
+
+	//构造函数
+	ListIterator(pNode node)
+		:_node(node){
+
+	}
+
+	//前置++  _node = _node.next
+	//后置++
+	//返回的是迭代器本身
+	self operator++(){
+		return _node = _node->_next;
+	}
+	self oprator++(int){
+		//拷贝构造一个++之前的迭代器
+		Self tmp(*this);
+		_node = _node->_next;
+		return tmp;
+	}
+
+
+
+	//--   _node = _node.prev
+	//解引用
+	// ->
+	// != _node != itrator._node
+
 
 
 };
@@ -33,7 +64,8 @@ class List{
 public:
 	typedef ListNode<T> Node;
 	typedef Node* pNode;
-	typedef ListIterator iterator;
+	typedef ListIterator<T, T&, T*> iterator;
+	typedef ListIterator<T, const T&, const T*> const_iterator;
 
 	List(){
 		createList();
@@ -44,7 +76,79 @@ public:
 		_head->_prev = _head;
 	}
 
+	//拷贝构造
+	List(const List<T>& val){
+		//创建头
+		createList();
+		for (const auto& e : val){
+			PushBack(e);
+		}
+	}
 
+	//迭代器构造
+	List(iterator first, iterator end){
+		//
+		createList();
+		while (first != end){
+			PushBack(*first);
+			++first;
+		}
+	}
+
+	template <class T>
+	void Swap(T& a, T& b){
+		T tmp = a;
+		a = b;
+		b = tmp;
+	}
+	//赋值运算符重载 // 深拷贝
+	List<T>& operator=(List<T> val){
+		Swap(_head, val._head);//拷贝构造, 交换头
+		return *this;
+	}
+
+	//插入 删除
+	void PushFront(const T& val);
+	void PushBack(const T& val);
+
+	void Insert(iterator pos, const T& val);
+	iterator Erase(iterator pos){
+		//头节点不能删 
+	}
+
+	void PopBack();
+	void PopFront();
+
+	//是否为空
+	bool Empty(){
+		return _head == _head->_next;
+	}
+	//
+	size_t Size(){
+		size_t sz = 0;
+		for (auto e : _head->_next){
+			++sz;
+		}
+		return sz;
+	}
+
+	//清空
+	void Clear(){
+		if (!Empty()){
+			iterator it = begin();
+			while (it != end()){
+				it = Erase(it);
+			}
+		}
+	}
+
+	~List(){
+		Clear();
+		if (_head){
+			delete _head;
+			_head = nullptr;
+		}
+	}
 private:
 	pNode _head;
 };
