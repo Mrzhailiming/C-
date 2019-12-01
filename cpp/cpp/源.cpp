0cc1c3191e49af46d356f19db1dd2b1e77b1ee69
Mrzhailiming <401875751@qq.com>
2019-12-01 22:24:27 +0800

    第一次出现的字符

--- cpp/cpp/源.cpp
+++ cpp/cpp/源.cpp
@@ -33,62 +33,96 @@
 
 
 
+//#include <iostream>
+//#include <string.h>
+//using namespace std;
+//
+//
+//int maxDiv(int a, int b){
+//	int ret = 0;
+//	int max = a > b ? a : b;
+//	int min = a > b ? b : a;
+//	while (max % min != 0){
+//		ret = max % min;
+//		max = min;
+//		min = ret;
+//	}
+//	return min;
+//}
+//
+//int begin(int now, int n, int* pow){
+//	int i = 0;
+//	int ret = now;
+//	while (n--){
+//		if (pow[i] <= ret){
+//			ret += pow[i];
+//		}
+//		else{
+//			int maxdiv = maxDiv(pow[i], ret);
+//			ret += maxdiv;
+//		}
+//		++i;
+//	}
+//	return ret;
+//}
+//
+//
+//
+//
+//
+//int main(){
+//	int now, num, fin;
+//	int* pow = new int[100000];
+//	memset(pow, 0, 100000);
+//	while (cin >> num >> now){
+//		int i = 0;
+//		int n = num;
+//		while (n--){
+//			cin >> pow[i++];
+//		}
+//
+//		fin = begin(now, num, pow);
+//		cout << fin << endl;
+//	}
+//
+//	return 0;
+//}
+
+
 #include <iostream>
-#include <string.h>
 using namespace std;
 
 
-int maxDiv(int a, int b){
-	int ret = 0;
-	int max = a > b ? a : b;
-	int min = a > b ? b : a;
-	while (max % min != 0){
-		ret = max % min;
-		max = min;
-		min = ret;
+void pri(char* str){
+	int arr[128] = { 0 };
+	char* s = str;
+	while (*s != '\0'){
+		arr[*s]++;
+		++s;
 	}
-	return min;
-}
-
-int begin(int now, int n, int* pow){
 	int i = 0;
-	int ret = now;
-	while (n--){
-		if (pow[i] <= ret){
-			ret += pow[i];
+	s = str;
+	for (i = 0; i < 128; ++i){
+		if (arr[*s] == 1){
+			cout << *s << endl;
+			return;
 		}
-		else{
-			int maxdiv = maxDiv(pow[i], ret);
-			ret += maxdiv;
-		}
-		++i;
+		++s;
 	}
-	return ret;
+	cout << -1 << endl;
 }
-
-
-
-
 
 int main(){
-	int now, num, fin;
-	int* pow = new int[100000];
-	memset(pow, 0, 100000);
-	while (cin >> num >> now){
-		int i = 0;
-		int n = num;
-		while (n--){
-			cin >> pow[i++];
-		}
+	char str[128] = "";
+	cin >> str;
 
-		fin = begin(now, num, pow);
-		cout << fin << endl;
-	}
+	pri(str);
+
+
+
 
 	return 0;
 }
-
-
 
 
 
