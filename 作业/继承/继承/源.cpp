242790c2e529b8ae7dbb3410b80bf0236d16db5c
Mrzhailiming <401875751@qq.com>
2020-02-09 13:19:51 +0800

    继承作业题

---
+++ 作业/继承/继承/源.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Car{
+public:
+	void fun1(int a){
+		;
+	}
+	void fun1(){
+		;
+	}
+private:
+	int a;
+	double a;
+};
+
+
+
+int main(){
+
+
+
+
+	return 0;
+}
