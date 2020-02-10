19866df56b73621f2a74d3ca4a5d174849df6ac0
Mrzhailiming <401875751@qq.com>
2020-02-10 10:36:36 +0800

    multimap之equal_range

---
+++ 树形结构的关联式容器/树形结构的关联式容器/main.cpp
@@ -0,0 +1,34 @@
+#include <map>
+#include <iostream>
+using namespace std;
+
+void testMultimap(){
+	multimap<char, int> mp;
+
+	mp.insert(make_pair('a', 10));
+	mp.insert(make_pair('a', 10));
+	mp.insert(make_pair('a', 10));
+	mp.insert(make_pair('a', 10));
+	mp.insert(make_pair('a', 10));
+	mp.insert(make_pair('b', 10));
+	mp.insert(make_pair('b', 10));
+	mp.insert(make_pair('c', 10));
+	//pair<multimap<char, int>::iterator, multimap<char, int>::iterator> ret;
+	auto ret = mp.equal_range('a');
+	while (ret.first != ret.second){
+		cout << ret.first->first << endl;
+		++ret.first;
+
+	}
+
+
+}
+
+
+int main(){
+	testMultimap();
+
+
+
+	return 0;
+}
