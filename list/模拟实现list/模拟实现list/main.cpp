1dccc3e17863b53e9a51f9a90c0a5e7dfec2a276
Mrzhailiming <401875751@qq.com>
2020-03-09 20:35:11 +0800

    1

--- list/模拟实现list/模拟实现list/main.cpp
+++ list/模拟实现list/模拟实现list/main.cpp
@@ -2,7 +2,12 @@
 template<class T>
 struct ListNode
 {
-	ListNode(const T& val = T());
+	ListNode(const T& val = T())
+	: _val(val)
+	, _pPre(nullptr)
+	, _pNext(nullptr){
+		;
+	}
 	ListNode<T>* _pPre;
 	ListNode<T>* _pNext;
 	T _val;
@@ -14,17 +19,46 @@ class ListIterator
 	typedef ListNode<T>* PNode;
 	typedef ListIterator<T, Ref, Ptr> Self;
 public:
-	ListIterator(PNode pNode = nullptr)；
-		ListIterator(const Self& l)；
-		T& operator*()；
-		T* operator->()；
-		Self& operator++()；
-		Self operator++(int)；
-		Self& operator--();
-	Self& operator--(int);
-	bool operator!=(const Self& l)；
-		bool operator==(const Self& l)；
-private:
+	ListIterator(PNode pNode = nullptr)
+		:_pNode(pNode){
+		;
+	}
+	ListIterator(const Self& l)
+		: _pNode(l._pNode){
+		;
+	}
+	//返回val的引用
+	T& operator*(){
+		return _pNode->_val;
+	}
+	//返回val的地址
+	T* operator->(){
+		return &(_pNode->_val);
+	}
+	Self& operator++(){
+		_pNode = _pNode->_pNext;
+		return *this;
+	}
+	Self operator++(int){
+		Self tmp = *this;
+		_pNode = _pNode->_pNext;
+		return tmp;
+	}
+	Self& operator--(){
+		_pNode = _pNode->_pPre;
+		return *this;
+	}
+	Self& operator--(int){
+		Self tmp = *this;
+		_pNode = _pNode->_pNext;
+		return tmp;
+	}
+	bool operator!=(const Self& l){
+		return _pNode != l->_pNode;
+	}
+	bool operator==(const Self& l){
+		return _pNode == l->_pNode;
+	}
 	PNode _pNode;
 };
 //list类
@@ -39,42 +73,127 @@ public:
 public:
 	///////////////////////////////////////////////////////////////
 	// List的构造
-	list();
-	list(int n, const T& value = T());
+	list(){
+		CreateHead();
+	}
+	list(int n, const T& value = T()){
+
+	}
 	template <class Iterator>
-	list(Iterator first, Iterator last);
-	list(const list<T>& l);
-	list<T>& operator=(const list<T> l);
-	~list();
+	list(Iterator first, Iterator last){
+
+	}
+	list(const list<T>& l){
+
+	}
+	list<T>& operator=(const list<T> l){
+
+	}
+	~list(){
+
+	}
 	///////////////////////////////////////////////////////////////
 	// List Iterator
-	iterator begin();
-	iterator end();
-	const_iterator begin();
-	const_iterator end();
+	iterator begin(){
+		return iterator(_pHead->_pNext);
+	}
+	iterator end(){
+		return iterator(_pHead);
+	}
+	const_iterator const_begin(){
+		return const_iterator(_pHead->_pNext);
+	}
+	const_iterator const_end(){
+		return const_iterator(_pHead);
+	}
 	///////////////////////////////////////////////////////////////
 	// List Capacity
-	size_t size()const;
-	bool empty()const;
+	size_t size()const{
+
+	}
+	bool empty()const{
+		return _pHead->_pNext == _pHead;
+	}
 	////////////////////////////////////////////////////////////
 	// List Access
-	T& front();
-	const T& front()const;
-	T& back();
-	const T& back()const;
+	T& front(){
+		return _pHead->_pNext->_val;
+	}
+	const T& front()const{
+		return _pHead->_pNext->_val;
+	}
+	T& back(){
+		return _pHead->_pPre;
+	}
+	const T& back()const{
+		return _pHead->_pPre;
+	}
 	////////////////////////////////////////////////////////////
 	// List Modify
-	void push_back(const T& val){ insert(begin(), val); }
-	void pop_back(){ erase(--end()); }
-	void push_front(const T& val){ insert(begin(), val); }
-	void pop_front(){ erase(begin()); }
+	void push_back(const T& val){ 
+		insert(--end(), val);
+	}
+	void pop_back(){ 
+		erase(--end()); 
+	}
+	void push_front(const T& val){ 
+		insert(begin(), val);
+	}
+	void pop_front(){ 
+		erase(begin()); 
+	}
 	// 在pos位置前插入值为val的节点
-	iterator insert(iterator pos, const T& val);
+	iterator insert(iterator pos, const T& val){
+		PNode pre = pos._pNode->_pPre;
+		PNode next = pos._pNode;
+		PNode newNode = new Node(val);
+		pre->_pNext = newNode;
+		newNode->_pPre = pre;
+		newNode->_pNext = next;
+		next->_pPre = newNode;
+		return iterator(newNode);
+	}
 	// 删除pos位置的节点，返回该节点的下一个位置
-	iterator erase(iterator pos);
-	void clear();
-	void swap(List<T>& l);
+	iterator erase(iterator pos){
+		PNode pre = pos._pNode->_pPre;
+		PNode next = pos._pNode->_pNext;
+		pre->_pNext = next;
+		next->_pPre = pre;
+		delete pos->_pNode;
+		pos->_pNode = nullptr;
+		return iterator(next);
+	}
+	void clear(){
+		if (_pHead != _pHead->_pNext){
+			PNode cur = _pHead->_pNext;
+			PNode next = cur->_pNext;
+			while (next != _pHead){
+				delete cur;
+				cur = next;
+				next = next->_pNext;
+			}
+			delete cur;
+			cur = nullptr;
+		}
+	}
+	void swap(list<T>& l){
+
+	}
 private:
-	void CreateHead();
+	void CreateHead(){
+		_pHead = new Node();
+		_pHead->_pNext = _pHead;
+		_pHead->_pPre = _pHead;
+		_pHead->_val = T();
+	}
 	PNode _pHead;
 };
+
+int main(){
+	list<int> l;
+	l.insert(l.begin(), 1);
+	l.insert(l.begin(), 2);
+	l.insert(l.begin(), 3);
+	l.front();
+	return 0;
+}
