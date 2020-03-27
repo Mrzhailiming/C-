d757697f6d1987b1a120f37642686cf724472147
Mrzhailiming <401875751@qq.com>
2020-03-27 14:27:58 +0800

    hushBucket

--- 树形结构的关联式容器/练习/hashTable/hashTable/hushBucket.cpp
+++ 树形结构的关联式容器/练习/hashTable/hashTable/hushBucket.cpp
@@ -75,8 +75,77 @@ public:
 	//	}
 	//}
 
-	
-
+	bool insertNode(V val){
+		checkCapacity();
+		keyOfValue kov;
+		size_t index = kov(val) % _hash.size();
+		pNode cur = _hash[index];
+		while (cur){
+			if (kov(cur->_data) == kov(val)){
+				return false;
+			}
+			cur = cur->_next;
+		}
+		//头插
+		cur = new node(val);
+		cur->_next = _hash[index];
+		_hash[index] = cur;
+		++_size;
+		return true;
+	}
+	void checkCapacity(){
+		if (_size == _hash.size()){
+			size_t newC = _hash.size() * 2;
+			hashBucket<K, V, keyOfValue> newHush(newC);
+			size_t index = 0;
+			while (index < _hash.size()){
+				pNode cur = _hash[index];
+				keyOfValue kov;
+				while (cur){
+					pNode next = cur->_next;
+					//重新计算位置
+					size_t newIndex = kov(cur->_data) % newHush._hash.size();
+					//头插
+					cur->_next = newHush._hash[newIndex];
+					newHush._hash[newIndex] = cur;
+					cur = next;
+				}
+				_hash[index] = nullptr;
+				++index;
+			}
+			_hash.swap(newHush._hash);
+		}
+	}
+	bool eraseNode(K key){
+		size_t index = key % _hash.size();
+		keyOfValue kov;
+		if (_hash[index]){
+			pNode cur = _hash[index];
+			pNode prev = nullptr;
+			while (cur){
+				
+				//找到
+				if (kov(cur->_data) == key){
+					//为头结点
+					if (cur == _hash[index]){
+						_hash[index] = cur->_next;
+						delete cur;
+						cur = nullptr;
+						return true;
+					}
+					//不为头结点
+					else{
+						prev->_next = cur->_next;
+						delete cur;
+						return true;
+					}
+				}
+				prev = cur;
+				cur = cur->_next;
+			}
+		}
+		return false;
+	}
 	void printHash(){
 		size_t index = 0;
 		while (index < _hash.size()){
@@ -100,16 +169,20 @@ void testHushBucket(){
 	h.insertNode(1);
 	h.insertNode(2);
 	h.insertNode(3);
-	h.insertNode(77);
-	h.insertNode(88);
-	h.insertNode(33);
-	h.insertNode(44);
 	h.insertNode(4);
-	h.insertNode(66);
+	h.insertNode(5);
 	h.insertNode(6);
-	h.insertNode(56);
-	h.insertNode(78);
-	h.insertNode(22);
+	h.insertNode(9);
+	h.insertNode(10);
+	h.insertNode(11);
+	h.insertNode(12);
+	h.insertNode(13);
+	h.insertNode(14);
+	h.insertNode(17);
+	h.insertNode(32);
+	h.insertNode(16);
+	h.printHash();
+	h.eraseNode(9);
 	h.printHash();
 }
 
